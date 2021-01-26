cabc7052a7cfe344bebe5123998e4f519df9fb77
jdew89 <jadewitt00@gmail.com>
2021-01-26 10:07:38 -0500

    Added TechIcons and ee experience section

---
+++ components/TechIcon.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tooltip">
+        <fa :icon="fasIcon" class="icon is-medium tooltip" />
+        <span class="tooltiptext">
+            {{tooltipText}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        fasIcon: Array,
+        tooltipText: String,
+    }
+}
+</script>
+
+<style lang="scss" >
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  position: absolute;
+  width: 100px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  top: 100%;
+  left: 50%;
+  margin-left: -50px;
+  z-index: 1;
+}
+
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent black transparent;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,42 @@
       </div>
     </section>
     <section class="section">
+      <article>
+        <header>
+          <h1 class="title is-3"> Experience </h1>
+        </header>
+        <section class="section">
+          <header>
+            <h3 class="title is-5">
+              Easy Earnest
+            </h3>
+            <h4 class="subtitle is-6">
+              Co-Founder and Full Stack Engineer
+            </h4>
+          </header>
+          <div class="columns mt-2">
+            <div class="column">
+              <img class="image" src="~/assets/imgs/EE_Dashboard.png" alt="Easy Earnest Logo">
+            </div>
+            <div class="column">
+              <div class="content">
+                <p>
+                  Easy Earnest is an electronic earnest money payment platform for Real Estate transactions. This product solves the headaches involved with accepting paper checks for earnest money deposits.
+                  We are in the age of technology and yet we still <u>only</u> accept paper checks for the largest transaction of a person's life? Easy Earnest hopes to modernize the Real Estate industry by
+                  providing a platform for customers to use that is Fast, Secure and Easy!
+                </p>
+                <TechIcon :fasIcon="['fab', 'node-js']" tooltipText="NodeJS" />
+                <TechIcon :fasIcon="['fab', 'vuejs']" tooltipText="VueJS" />
+                <TechIcon :fasIcon="['fab', 'html5']" tooltipText="HTML5" />
+                <TechIcon :fasIcon="['fab', 'sass']" tooltipText="SASS" />
+                <TechIcon :fasIcon="['fab', 'aws']" tooltipText="AWS" />
+
+              </div>
+            </div>
+          </div>
+
+        </section>
+      </article>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ducimus, sed vel itaque magnam molestiae, incidunt dicta nisi tempore pariatur est laborum et exercitationem omnis. Placeat, alias. Cupiditate, ipsum accusamus.
       </p>
