382f082fa4d78e03aea86d73baeab0b7096ce55c
Jeff DeWitt <jadewitt00@gmail.com>
2021-01-28 16:58:00 -0500

    snowy hills at bottom

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,37 +1,42 @@
 <template>
-  <footer class="footer">
-      <div class="container">
-        <nav class="level">
-          <div class="level-item has-text-centered">
-            <NuxtLink to="/" class="button is-primary is-outlined"> Home </NuxtLink>
-          </div>
-          <div class="level-item has-text-centered">
-            <NuxtLink to="#portfolio" class="button is-link is-outlined"> 
-                Portfolio
-            </NuxtLink>
-          </div>
-          <div class="level-item has-text-centered">
-            <NuxtLink to="#about" class="button is-link is-outlined"> 
-                About
-            </NuxtLink>
-          </div>
-          <div class="level-right">
-            <div class="level-item">
-              <a href="https://github.com/jdew89" class="button is-rounded is-outlined is-link is-small" target="_blank">
-                <fa :icon="['fab', 'github']" class="icon is-large"/>
-              </a>
+  <div>
+    <footer class="footer">
+        <div class="container">
+          <nav class="level">
+            <div class="level-item has-text-centered">
+              <NuxtLink to="/" class="button is-primary is-outlined"> Home </NuxtLink>
             </div>
-            <div class="level-item">
-              <a href="https://www.linkedin.com/in/jeffrey-dewitt-902b0560/" class="button is-rounded is-outlined is-link is-small" target="_blank">
-                <fa :icon="['fab', 'linkedin']" class="icon is-large"/>
-              </a>
+            <div class="level-item has-text-centered">
+              <NuxtLink to="#portfolio" class="button is-link is-outlined"> 
+                  Portfolio
+              </NuxtLink>
             </div>
-          </div>
-        </nav>
-      </div>
-    </footer>
+            <div class="level-item has-text-centered">
+              <NuxtLink to="#about" class="button is-link is-outlined"> 
+                  About
+              </NuxtLink>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <a href="https://github.com/jdew89" class="button is-rounded is-outlined is-link is-small" target="_blank">
+                  <fa :icon="['fab', 'github']" class="icon is-large"/>
+                </a>
+              </div>
+              <div class="level-item">
+                <a href="https://www.linkedin.com/in/jeffrey-dewitt-902b0560/" class="button is-rounded is-outlined is-link is-small" target="_blank">
+                  <fa :icon="['fab', 'linkedin']" class="icon is-large"/>
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </footer>
+      <img class="image" src="/snow-hills-plain.svg" alt="" width="100%">
+  </div>
 </template>
 
 <style>
-
+.footer{
+  padding-bottom: 3rem;
+}
 </style>
