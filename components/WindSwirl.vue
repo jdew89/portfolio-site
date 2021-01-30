cca330de235da71eb6fc91318594678ec4b75408
Jeff DeWitt <jadewitt00@gmail.com>
2021-01-30 15:32:15 -0500

    Added wind swirls as a component

---
+++ components/WindSwirl.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <svg version="1.1" viewBox="0 0 211.67 17.198" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+      <defs>
+        <filter id="filter913" x="-.0051675" y="-.037481" width="1.0103" height="1.075" color-interpolation-filters="sRGB">
+        <feGaussianBlur stdDeviation="0.39094517"/>
+        </filter>
+      </defs>
+      <metadata>
+        <rdf:RDF>
+        <cc:Work rdf:about="">
+          <dc:format>image/svg+xml</dc:format>
+          <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
+          <dc:title/>
+        </cc:Work>
+        </rdf:RDF>
+      </metadata>
+      <g fill="none" stroke="#dae7f1" stroke-width="1.3229">
+        <path class="svg-path" transform="matrix(1.1971 0 0 .48112 -6.1605 .76772)" d="m185.44 17.524c-21.879 2.0828-43.758 4.1656-65.069 4.4036-21.311 0.23806-42.054-1.3687-53.917-4.2845-11.863-2.9158-14.847-7.1409-13.284-10.474 1.5629-3.3327 7.6719-5.7726 15.486-6.5461 7.8142-0.77353 17.333 0.11909 22.732 2.0533 5.3986 1.9342 6.6773 4.9096 5.4697 7.7361-1.2076 2.8265-4.9015 5.5043-11.721 7.9442s-16.764 4.6417-30.83 5.7724c-14.065 1.1307-32.25 1.1902-50.436 1.2497" filter="url(#filter913)"/>
+        <path class="svg-path" transform="matrix(1.1971 0 0 .48112 -5.4817 1.9008)" d="m185.44 17.524c-21.879 2.0828-43.758 4.1656-65.069 4.4036-21.311 0.23806-42.054-1.3687-53.917-4.2845-11.863-2.9158-14.847-7.1409-13.284-10.474 1.5629-3.3327 7.6719-5.7726 15.486-6.5461 7.8142-0.77353 17.333 0.11909 22.732 2.0533 5.3986 1.9342 6.6773 4.9096 5.4697 7.7361-1.2076 2.8265-4.9015 5.5043-11.721 7.9442s-16.764 4.6417-30.83 5.7724c-14.065 1.1307-32.25 1.1902-50.436 1.2497" filter="url(#filter913)"/>
+        <path class="svg-path" transform="matrix(1.1971 0 0 .48112 -5.0292 3.1912)" d="m185.44 17.524c-21.879 2.0828-43.758 4.1656-65.069 4.4036-21.311 0.23806-42.054-1.3687-53.917-4.2845-11.863-2.9158-14.847-7.1409-13.284-10.474 1.5629-3.3327 7.6719-5.7726 15.486-6.5461 7.8142-0.77353 17.333 0.11909 22.732 2.0533 5.3986 1.9342 6.6773 4.9096 5.4697 7.7361-1.2076 2.8265-4.9015 5.5043-11.721 7.9442s-16.764 4.6417-30.83 5.7724c-14.065 1.1307-32.25 1.1902-50.436 1.2497" filter="url(#filter913)"/>
+      </g>
+      </svg>
+    </div>
+</template>
+
+<style lang="scss">
+.svg-path{
+  opacity: 0.2;
+  stroke-dasharray: 200;
+  stroke-dashoffset: 0;
+  animation: dash 20s linear infinite;
+}
+
+@keyframes dash {
+  to{
+    stroke-dashoffset: 2000;
+  }
+}
+</style>
